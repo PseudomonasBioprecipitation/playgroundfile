5b5e5b9c40fa8e319988343d588b7119e821990d
Pseudomonas Bioprecipitation <pseudomonasbioprecipitation@outlook.com>
2023-12-17 17:02:55 -0300

    Update calculator.js

--- calculator.js
+++ calculator.js
@@ -1,6 +1,10 @@
+function difference(num1, num2) {
+  return Math.abs(num1 - num2);
+}
+
 function calcDifference() {
-    var num1 = parseInt(document.getElementById('num1').value);
-    var num2 = parseInt(document.getElementById('num2').value);
-    var result = difference(num1, num2);
-    document.getElementById('result').value = result;
-  }
+  var num1 = parseInt(document.getElementById('num1').value);
+  var num2 = parseInt(document.getElementById('num2').value);
+  var result = difference(num1, num2);
+  document.getElementById('result').value = result;
+}
