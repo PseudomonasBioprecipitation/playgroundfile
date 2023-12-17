6dc4695be6fc2c4a5facdd47a86c056f0eea35a8
PseudomonasBioprecipitation <151096845+PseudomonasBioprecipitation@users.noreply.github.com>
2023-12-17 15:44:39 -0300

    Updated file path

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ if (selectedSet === 'set1') {
   // Load files and content for Set 1
   var link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = 'set1.css';
+  link.href = 'playgroundfile/set1.css';
   document.head.appendChild(link);
-  window.location.href = 'set1.html';
+  window.location.href = 'playgroundfile/set1.html';
 } else if (selectedSet === 'set2') {
   // Load files and content for Set 2
   var link = document.createElement('link');
   link.rel = 'stylesheet';
-  link.href = 'set2.css';
+  link.href = 'playgroundfile/set2.css';
   document.head.appendChild(link);
-  window.location.href = 'set2.html';
+  window.location.href = 'playgroundfile/set2.html';
 }
