df8c17adfd87b1d17062f84f78f783e154d2dcf4
Pseudomonas Bioprecipitation <pseudomonasbioprecipitation@outlook.com>
2023-12-17 16:44:11 -0300

    Update script.js
    Update

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 document.querySelector('form').addEventListener('submit', function(event) {
   event.preventDefault();
   var selectedSet = document.querySelector('input[name="set"]:checked').value;
-if (selectedSet === 'set1') {
-  // Load files and content for Set 1
   var link = document.createElement('link');
   link.rel = 'stylesheet';
-  window.href = 'playgroundfile/set1.css';
-  document.head.appendChild(link);
-  window.location.href = 'playgroundfile/set1.html';
-} else if (selectedSet === 'set2') {
-  // Load files and content for Set 2
-  var link = document.createElement('link');
-  link.rel = 'stylesheet';
-  window.href = 'playgroundfile/set2.css';
-  document.head.appendChild(link);
-  window.location.href = 'playgroundfile/set2.html';
-}
+
+  if (selectedSet === 'set1') {
+    // Load files and content for Set 1
+    link.href = 'playgroundfile/set1.css';
+    document.head.appendChild(link);
+    window.location.href = 'playgroundfile/set1.html';
+  } else if (selectedSet === 'set2') {
+    // Load files and content for Set 2
+    link.href = 'playgroundfile/set2.css';
+    document.head.appendChild(link);
+    window.location.href = 'playgroundfile/set2.html';
+  }
+});
