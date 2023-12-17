c0806e64be8f1626b3ef7ecc91cda92b68a8685d
PseudomonasBioprecipitation <151096845+PseudomonasBioprecipitation@users.noreply.github.com>
2023-12-17 14:37:32 -0300

    Add all updated files

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 document.querySelector('form').addEventListener('submit', function(event) {
   event.preventDefault();
   var selectedSet = document.querySelector('input[name="set"]:checked').value;
-  if (selectedSet === 'set1') {
-    // Load files and content for Set 1
-    var script = document.createElement('script');
-    script.src = 'set1.js';
-    document.head.appendChild(script);
-    console.log('Loading Set 1');
-  } else if (selectedSet === 'set2') {
-    // Load files and content for Set 2
-    var script = document.createElement('script');
-    script.src = 'set2.js';
-    document.head.appendChild(script);
-    console.log('Loading Set 2');
-  }
-});
+if (selectedSet === 'set1') {
+  // Load files and content for Set 1
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'set1.css';
+  document.head.appendChild(link);
+  window.location.href = 'set1.html';
+} else if (selectedSet === 'set2') {
+  // Load files and content for Set 2
+  var link = document.createElement('link');
+  link.rel = 'stylesheet';
+  link.href = 'set2.css';
+  document.head.appendChild(link);
+  window.location.href = 'set2.html';
+}
