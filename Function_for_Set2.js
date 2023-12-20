bf4db79490b90b50c1862616d211eb13119524d7
PseudomonasBioprecipitation <151096845+PseudomonasBioprecipitation@users.noreply.github.com>
2023-12-20 16:05:48 -0300

    Function for Set2 updated to calculate float numbers

--- Function_for_Set2.js
+++ Function_for_Set2.js
@@ -1,6 +1,12 @@
-function calcDifference() {
+/*function calcDifference() {
   var num1 = parseInt(document.getElementById('num1').value);
   var num2 = parseInt(document.getElementById('num2').value);
   var result = Math.abs(num1 - num2);
+  document.getElementById('result').value = result;
+}*/
+function calcDifference() {
+  var num1 = parseFloat(document.getElementById('num1').value);
+  var num2 = parseFloat(document.getElementById('num2').value);
+  var result = num1 - num2;
   document.getElementById('result').value = result;
 }
